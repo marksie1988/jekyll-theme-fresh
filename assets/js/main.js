b741002295838638147a1f590272e86e8e704470
Steven Marks <steven.marks@rackspace.com>
2020-12-09 10:07:56 +0000

    fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,10 +12,10 @@ reframe(videoEmbeds.join(','));
 // Mobile menu
 var menuToggle = document.getElementById('menu-toggle');
 if (menuToggle) {
-  menuToggle.addEventListener('click', function (e) {
+  menuToggle.addEventListener('click', function(e){
     document.body.classList.toggle('menu--opened');
     e.preventDefault();
-  }, false);
+  },false);
   document.body.classList.remove('menu--opened');
 }
 
